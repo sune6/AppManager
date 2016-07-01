51297c5a6cf474b482722d3a4778e2484eb1e440
Sun <sune6@qq.com>
2016-07-01 14:45:10 +0800

    没啥改动

--- app/src/main/java/com/sune/appmanager/AppInfo.java
+++ app/src/main/java/com/sune/appmanager/AppInfo.java
@@ -1,36 +1,13 @@
 package com.sune.appmanager;
 
-import java.text.Collator;
-import java.util.Comparator;
-
 import android.graphics.drawable.Drawable;
 
 public class AppInfo {
-	public Drawable icon;
-	public String packageName;
-	public String appName;
-	public long size;
 
-	static class ProgramName implements Comparator<AppInfo> {
-		private Collator collator = Collator.getInstance();
+    public Drawable icon;
 
-		public int compare(AppInfo paramProgram1, AppInfo paramProgram2) {
-			return this.collator.compare(paramProgram1.appName,
-					paramProgram2.appName);
-		}
-	}
+    public String appName;
 
-	static class ProgramSize implements Comparator<AppInfo> {
-		public int compare(AppInfo paramProgram1, AppInfo paramProgram2) {
-			boolean bool = paramProgram1.size < paramProgram2.size;
-			int i = 0;
-			if (bool)
-				i = 1;
-			if (paramProgram1.size == paramProgram2.size)
-				i = 0;
-			if (paramProgram1.size < paramProgram2.size)
-				i = -1;
-			return i;
-		}
-	}
+    public String packageName;
+
 }
--- app/src/main/java/com/sune/appmanager/AppUtil.java
+++ app/src/main/java/com/sune/appmanager/AppUtil.java
@@ -5,40 +5,55 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
+import android.support.annotation.NonNull;
 
 public class AppUtil {
-	
-	public static void showAppDetails(Context context, String pkg) {
-		int i = Build.VERSION.SDK_INT;
-		Intent intent = new Intent();
-		if (i <= 7) {
-			intent.setAction("android.intent.action.VIEW");
-			intent.setClassName("com.android.settings", "com.android.settings.InstalledAppDetails");
-			intent.putExtra("com.android.settings.ApplicationPkgName", pkg);
-		}
-		if (i == 8) {
-			intent.setAction("android.intent.action.VIEW");
-			intent.setClassName("com.android.settings", "com.android.settings.InstalledAppDetails");
-			intent.putExtra("pkg", pkg);
-		} else if (i >= 9) {
-			intent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
-			intent.setData(Uri.fromParts("package", pkg, null));
-		}
-		context.startActivity(intent);
-	}
 
-	public static void startApp(Context context, String pkg) {
-		PackageManager localPackageManager = context.getPackageManager();
-		new Intent("android.intent.action.VIEW");
-		Intent localIntent = localPackageManager
-				.getLaunchIntentForPackage(pkg);
-		context.startActivity(localIntent);
-	}
+    /**
+     * 显示 系统设置-应用管理-app详情 界面
+     *
+     * @param context context
+     * @param pkg app包名
+     */
+    public static void showAppDetails(@NonNull Context context, @NonNull String pkg) {
+        int version = Build.VERSION.SDK_INT;
+        Intent intent = new Intent();
+        if (version <= 7) {
+            intent.setAction("android.intent.action.VIEW");
+            intent.setClassName("com.android.settings", "com.android.settings.InstalledAppDetails");
+            intent.putExtra("com.android.settings.ApplicationPkgName", pkg);
+        }
+        if (version == 8) {
+            intent.setAction("android.intent.action.VIEW");
+            intent.setClassName("com.android.settings", "com.android.settings.InstalledAppDetails");
+            intent.putExtra("pkg", pkg);
+        } else if (version >= 9) {
+            intent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+            intent.setData(Uri.fromParts("package", pkg, null));
+        }
+        context.startActivity(intent);
+    }
 
+    /**
+     * 启动应用
+     *
+     * @param context context
+     * @param pkg app包名
+     */
+    public static void startApp(@NonNull Context context, @NonNull String pkg) {
+        PackageManager localPackageManager = context.getPackageManager();
+        Intent localIntent = localPackageManager.getLaunchIntentForPackage(pkg);
+        context.startActivity(localIntent);
+    }
 
-	public static void uninstallApp(Context context, String pkg) {
-		Intent localIntent = new Intent("android.intent.action.DELETE",
-				Uri.parse("package:" + pkg));
-		context.startActivity(localIntent);
-	}
+    /**
+     * 显示app卸载界面
+     *
+     * @param context context
+     * @param pkg app包名
+     */
+    public static void uninstallApp(@NonNull Context context, @NonNull String pkg) {
+        Intent localIntent = new Intent("android.intent.action.DELETE", Uri.parse("package:" + pkg));
+        context.startActivity(localIntent);
+    }
 }
